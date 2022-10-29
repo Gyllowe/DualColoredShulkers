79b8018c568b20cb74c63226db00951fea45fd72
Gyllowe <Andre.Pettersen.2005@gmail.com>
2022-10-29 15:02:59 +0200

    0.0.3 - Rendering rewrite, new command and code cleanup Rewrote rendering to be less intrusive and probably better performing. Added command "/dualshulkers block" to check and change the color of placed shulker boxes.   - Changing lid color is not implemented   - The rendering doesn't update cleaned up project by organizing, structuring and deleting some files.

--- src/main/java/net/gyllowe/dualcoloredshulkers/_wip/Rendering_FirstAttempt/interfaces/ModelPartCuboidSetShulkerBase.java
+++
@@ -1,5 +0,0 @@
-package net.gyllowe.dualcoloredshulkers._wip.Rendering_FirstAttempt.interfaces;
-
-public interface ModelPartCuboidSetShulkerBase {
-    ModelPartCuboidSetShulkerBase SetShulkerBase();
-}
