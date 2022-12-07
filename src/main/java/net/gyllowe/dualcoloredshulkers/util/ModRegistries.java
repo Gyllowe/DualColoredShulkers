0d1e34863ca456eca15942cb08857b9ae14ba7cd
Gyllowe <Andre.Pettersen.2005@gmail.com>
2022-12-07 21:46:18 +0100

    Beta 0.1.1 - Removing unused things

--- src/main/java/net/gyllowe/dualcoloredshulkers/util/ModRegistries.java
+++ src/main/java/net/gyllowe/dualcoloredshulkers/util/ModRegistries.java
@@ -1,36 +1,10 @@
 package net.gyllowe.dualcoloredshulkers.util;
 
-import net.fabricmc.fabric.api.command.v2.ArgumentTypeRegistry;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.gyllowe.dualcoloredshulkers.commands.DualShulkerBlockCommand;
-import net.gyllowe.dualcoloredshulkers.commands.DualShulkerColorArgumentType;
-import net.gyllowe.dualcoloredshulkers.commands.ShulkerPartArgumentType;
-import net.minecraft.command.argument.serialize.ConstantArgumentSerializer;
-import net.minecraft.util.Identifier;
-
-import static net.gyllowe.dualcoloredshulkers.DualColoredShulkers.MOD_ID;
-
 public abstract class ModRegistries {
 	public static void RegisterAll() {
-		RegisterCommandsAndArgumentTypes();
 		RegisterLootTableModifierEvents();
 	}
 
-
-	private static void RegisterCommandsAndArgumentTypes() {
-		ArgumentTypeRegistry.registerArgumentType(
-				new Identifier(MOD_ID, "dyecolor"),
-				DualShulkerColorArgumentType.class,
-				ConstantArgumentSerializer.of(DualShulkerColorArgumentType::dualShulkerColor)
-		);
-		ArgumentTypeRegistry.registerArgumentType(
-				new Identifier(MOD_ID, "shulkerpart"),
-				ShulkerPartArgumentType.class,
-				ConstantArgumentSerializer.of(ShulkerPartArgumentType::shulkerPart)
-		);
-		CommandRegistrationCallback.EVENT.register(DualShulkerBlockCommand::register);
-		//CommandRegistrationCallback.EVENT.register(DualShulkerHoldingCommand::register);
-	}
 
 	private static void RegisterLootTableModifierEvents() {
 		LootTableRegistration.RegisterShulkerBoxLootTableEvent();
